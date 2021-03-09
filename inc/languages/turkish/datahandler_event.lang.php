831a03a077b91617d089d873176e2ba8bdc52430
Medet Erdal <tedemdev@gmail.com>
2021-03-09 12:04:42 +0300

    feat: done datahandler_event.lang.php

--- inc/languages/turkish/datahandler_event.lang.php
+++ inc/languages/turkish/datahandler_event.lang.php
@@ -1,34 +1,34 @@
 <?php
+
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Turkish Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['eventdata_missing_name'] = 'The name for the event is missing. Please enter an event name.';
-$l['eventdata_missing_description'] = 'The description for the event is missing. Please enter an event description.';
+$l['eventdata_missing_name'] = 'Etkinliğin adı eksik. Lütfen bir etkinlik adı girin.';
+$l['eventdata_missing_description'] = 'Etkinliğin açıklaması eksik. Lütfen bir etkinlik açıklaması girin.';
 
-$l['eventdata_invalid_start_date'] = 'The event start date you entered is invalid. You need to make sure you specify the day, month and year as well as ensure the day you entered is valid for the particular month.';
-$l['eventdata_invalid_start_year'] = "Events can only be created in the next 5 years. Please select a reasonable start year from the list.";
-$l['eventdata_invalid_start_month'] = 'The start month you entered is not a valid month. Please enter a valid start month.';
+$l['eventdata_invalid_start_date'] = 'Girdiğiniz etkinlik başlangıç tarihi geçersiz. Gün, ay ve yılı belirlediğinizden ve girdiğiniz günün belirli bir ay için geçerli olduğundan emin olmanız gerekir.';
+$l['eventdata_invalid_start_year'] = "Etkinlikler yalnızca önümüzdeki 5 yıl içinde oluşturulabilir. Lütfen listeden makul bir başlangıç yılı seçin.";
+$l['eventdata_invalid_start_month'] = 'Girdiğiniz başlangıç ayı geçerli bir ay değil. Lütfen geçerli bir başlangıç ayı girin.';
 
-$l['eventdata_invalid_end_date'] = 'The event end date you entered is invalid. You need to make sure you specify the day, month and year as well as ensure the day you entered is valid for the particular month.';
-$l['eventdata_invalid_end_year'] = "Events can only be created in the next 5 years. Please select a reasonable end year from the list.";
-$l['eventdata_invalid_end_month'] = 'The end month you entered is not a valid month. Please and enter a valid end month.';
-$l['eventdata_invalid_end_day'] = 'The end day you entered is not a valid day. The day you selected is probably greater than the number of days in this month.';
+$l['eventdata_invalid_end_date'] = 'Girdiğiniz etkinlik bitiş tarihi geçersiz. Gün, ay ve yılı belirttiğinizden ve girdiğiniz günün belirli bir ay için geçerli olduğundan emin olmanız gerekir.';
+$l['eventdata_invalid_end_year'] = "Etkinlikler yalnızca önümüzdeki 5 yıl içinde oluşturulabilir. Lütfen listeden makul bir bitiş yılı seçin.";
+$l['eventdata_invalid_end_month'] = 'Girdiğiniz bitiş ayı geçerli bir ay değil. Lütfen ve geçerli bir bitiş ayı girin.';
+$l['eventdata_invalid_end_day'] = 'Girdiğiniz bitiş günü, geçerli bir gün değil. Seçtiğiniz gün muhtemelen bu aydaki gün sayısından daha büyük.';
 
-$l['eventdata_cant_specify_one_time'] = "If you are specifying an event start time you need to enter an event end time.";
-$l['eventdata_start_time_invalid'] = "The start time you entered is invalid. Valid examples are 12am, 12:01am, 00:01.";
-$l['eventdata_end_time_invalid'] = "The end time you entered is invalid. Valid examples are 12am, 12:01am, 00:01.";
-$l['eventdata_invalid_timezone'] = "The time zone you selected for this event is invalid.";
-$l['eventdata_end_in_past'] = "The end date or time for your event is before the starting date or time.";
+$l['eventdata_cant_specify_one_time'] = "Bir etkinlik başlangıç zamanı belirliyorsanız, bir etkinlik bitiş zamanı girmeniz gerekir.";
+$l['eventdata_start_time_invalid'] = "Girdiğiniz başlangıç zamanı geçersiz. Geçerli örnekler 00:00, 12:01, 00:01'dir.";
+$l['eventdata_end_time_invalid'] = "Girdiğiniz bitiş saati geçersiz. Geçerli örnekler 00:00, 12:01, 00:01'dir.";
+$l['eventdata_invalid_timezone'] = "Bu etkinlik için seçtiğiniz saat dilimi geçersiz.";
+$l['eventdata_end_in_past'] = "Etkinliğinizin bitiş tarihi veya saati, başlangıç tarihi veya saatinden önce.";
 
-$l['eventdata_only_ranged_events_repeat'] = "Only ranged events (events with a start and finish date) can repeat.";
-$l['eventdata_invalid_repeat_day_interval'] = "You have entered an invalid day repeat interval.";
-$l['eventdata_invalid_repeat_week_interval'] = "You have entered an invalid week repeat interval.";
-$l['eventdata_invalid_repeat_weekly_days'] = "You did not select any weekdays for this event to occur on.";
-$l['eventdata_invalid_repeat_month_interval'] = "You have entered an invalid month repeat interval.";
-$l['eventdata_invalid_repeat_year_interval'] = "You have entered an invalid year repeat interval.";
-$l['eventdata_event_wont_occur'] = "Using the start and end times along with the event repetition settings, this event won't occur.";
-
-$l['eventdata_no_permission_private_event'] = "You do not have permission to post private events.";
+$l['eventdata_only_ranged_events_repeat'] = "Yalnızca aralıklı etkinlikler (bir başlangıç ve bitiş tarihi olan etkinlikler) tekrarlanabilir.";
+$l['eventdata_invalid_repeat_day_interval'] = "Geçersiz bir gün tekrar aralığı girdiniz.";
+$l['eventdata_invalid_repeat_week_interval'] = "Geçersiz bir hafta tekrar aralığı girdiniz.";
+$l['eventdata_invalid_repeat_weekly_days'] = "Bu etkinliğin gerçekleşmesi için hafta içi günler seçmediniz.";
+$l['eventdata_invalid_repeat_month_interval'] = "Geçersiz bir ay tekrar aralığı girdiniz.";
+$l['eventdata_invalid_repeat_year_interval'] = "Geçersiz bir yıl tekrar aralığı girdiniz.";
+$l['eventdata_event_wont_occur'] = "Etkinlik tekrarlama ayarlarıyla birlikte başlangıç ve bitiş zamanlarını kullanarak bu etkinlik gerçekleşmez.";
+$l['eventdata_no_permission_private_event'] = "Özel etkinlik gönderme izniniz yok.";
