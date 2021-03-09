e971b0be844eee9b5c1a0af974eaeb2ae5dfe4c2
Medet Erdal <tedemdev@gmail.com>
2021-03-09 11:13:46 +0300

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# MyBB Türkçe
+
+MyBB 1.8 Türkçe dil paketi.
+
+TDK yazım kurallarına uygun olarak çevirilmiştir.
+
+## Detaylar
+
+-   **Version:** 1.8.24
+
+## Gereksinimler
+
+-   MyBB = 1.8.24
+
+## Yazarlar
+
+-   **Medet Erdal** - _İlk çalışma_ - [tedem](https://github.com/tedem)
+
+## Lisans
+
+Bu proje MIT ile lisanslanmıştır. (Ayrıntılar için [LICENSE](LICENSE) dosyasına bakın.)
---
+++ inc/languages/turkish.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * MyBB 1.8 Turkish Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+// The friendly name of the language
+$langinfo['name'] = "Türkçe";
+
+// The author of the language
+$langinfo['author'] = "MyBB Group";
+
+// The language authors website
+$langinfo['website'] = "https://tedem.dev";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1823";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 1;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "tr";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
