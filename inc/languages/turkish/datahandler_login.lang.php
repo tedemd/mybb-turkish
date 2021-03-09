0af2dc00c7764fa476a6ef6a7c94b40dd185ea69
Medet Erdal <tedemdev@gmail.com>
2021-03-09 12:08:49 +0300

    feat: done datahandler_login.lang.php

--- inc/languages/turkish/datahandler_login.lang.php
+++ inc/languages/turkish/datahandler_login.lang.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Turkish Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['logindata_invalidpwordusername'] = "You have entered an invalid username/password combination. <br /><br />If you have forgotten your password please <a href=\"member.php?action=lostpw\">retrieve a new one</a>.";
-$l['logindata_invalidpwordusernameemail'] = "You have entered an invalid email/password combination. <br /><br />If you have forgotten your password please <a href=\"member.php?action=lostpw\">retrieve a new one</a>.";
-$l['logindata_invalidpwordusernamecombo'] = "You have entered an invalid username/password or email/password combination. <br /><br />If you have forgotten your password please <a href=\"member.php?action=lostpw\">retrieve a new one</a>.";
+$l['logindata_invalidpwordusername'] = "Geçersiz bir kullanıcı adı / şifre kombinasyonu girdiniz.<br /><br />Şifrenizi unuttuysanız, lütfen <a href=\"member.php?action=lostpw\">yenisini alın</a>.";
+$l['logindata_invalidpwordusernameemail'] = "Geçersiz bir e-posta / şifre kombinasyonu girdiniz.<br /><br />Şifrenizi unuttuysanız, lütfen <a href=\"member.php?action=lostpw\">yenisini alın</a>.";
+$l['logindata_invalidpwordusernamecombo'] = "Geçersiz bir kullanıcı adı / şifre veya e-posta / şifre kombinasyonu girdiniz.<br /><br />Şifrenizi unuttuysanız, lütfen <a href=\"member.php?action=lostpw\">yenisini alın</a>.";
 
-$l['logindata_regimageinvalid'] = "The image verification code that you entered was incorrect. Please enter the code exactly how it appears in the image.";
-$l['logindata_regimagerequired'] = "Please fill out the image verification code to continue the login process. Please enter the code exactly how it appears in the image.";
+$l['logindata_regimageinvalid'] = "Girdiğiniz resim doğrulama kodu yanlıştı. Lütfen kodu tam olarak resimde göründüğü gibi girin.";
+$l['logindata_regimagerequired'] = "Giriş işlemine devam etmek için lütfen resim doğrulama kodunu doldurun. Lütfen kodu tam olarak resimde göründüğü gibi girin.";
