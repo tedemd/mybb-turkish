f89e03d98bc3c10ecb13276de45bfa0ab3f62ffe
Medet Erdal <tedemdev@gmail.com>
2021-03-09 19:25:07 +0300

    feat: done helpsections.lang.php

--- inc/languages/turkish/helpsections.lang.php
+++ inc/languages/turkish/helpsections.lang.php
@@ -1,14 +1,15 @@
 <?php
+
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Turkish Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
 // Help Section 1
-$l['s1_name'] = "User Maintenance";
-$l['s1_desc'] = "Basic instructions for maintaining a forum account.";
+$l['s1_name'] = "Kullanıcı Bakımı";
+$l['s1_desc'] = "Bir forum hesabı bakımı için temel yönlendirmeler.";
 
 // Help Section 2
-$l['s2_name'] = "Posting";
-$l['s2_desc'] = "Posting, replying, and basic usage of forum.";
+$l['s2_name'] = "Gönderi";
+$l['s2_desc'] = "Gönderme, cevaplama ve forumun basit kullanımı.";
