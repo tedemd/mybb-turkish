cc312cda65231bb530a11e700056083f57dd837a
Medet Erdal <tedemdev@gmail.com>
2021-03-09 12:31:22 +0300

    feat: done datahandler_warnings.lang.php

--- inc/languages/turkish/datahandler_warnings.lang.php
+++ inc/languages/turkish/datahandler_warnings.lang.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Turkish Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['warnings_reached_max_warnings_day'] = "You cannot warn anyone as you have reached your limit on the number of warnings you can give out per day.<br /><br />The maximum number of warnings you can give out per day is {1}.";
-$l['warnings_error_invalid_user'] = "Selected user doesn't exist.";
-$l['warnings_error_invalid_post'] = "Selected post doesn't exist.";
-$l['warnings_error_cannot_warn_self'] = "You cannot add to your own warning level.";
-$l['warnings_error_user_reached_max_warning'] = "This user cannot be warned as they have already reached the maximum warning level.";
-$l['warnings_error_no_note'] = "You did not enter any administrative notes for this warning.";
-$l['warnings_error_invalid_type'] = "You have selected an invalid warning type.";
-$l['warnings_error_cant_custom_warn'] = "You do not have permission to give custom warnings to users.";
-$l['warnings_error_no_custom_reason'] = "You did not enter a reason for your custom warning.";
-$l['warnings_error_invalid_custom_points'] = "You did not enter a valid number of points to add to this users warning level. You need to enter a numeric value greater than 0 but not greater than {1}.";
-$l['warnings_error_invalid_expires_period'] = "You entered an invalid expiry type.";
+$l['warnings_reached_max_warnings_day'] = "Günlük verebileceğiniz uyarı sayısı sınırınıza ulaştığınız için kimseyi uyaramazsınız.<br /><br />Bir günde verebileceğiniz maksimum uyarı sayısı {1}'dır.";
+$l['warnings_error_invalid_user'] = "Seçili kullanıcı mevcut değil.";
+$l['warnings_error_invalid_post'] = "Seçili gönderi mevcut değil.";
+$l['warnings_error_cannot_warn_self'] = "Kendi uyarı seviyenize ekleme yapamazsınız.";
+$l['warnings_error_user_reached_max_warning'] = "Bu kullanıcı, maksimum uyarı seviyesine ulaştığı için uyarılamaz.";
+$l['warnings_error_no_note'] = "Bu uyarı için herhangi bir yönetim notu girmediniz.";
+$l['warnings_error_invalid_type'] = "Geçersiz bir uyarı türü seçtiniz.";
+$l['warnings_error_cant_custom_warn'] = "Kullanıcılara özel uyarılar verme izniniz yok.";
+$l['warnings_error_no_custom_reason'] = "Özel uyarınız için bir neden girmediniz.";
+$l['warnings_error_invalid_custom_points'] = "Bu kullanıcı uyarı düzeyine eklemek için geçerli bir puan sayısı girmediniz. 0'dan büyük ancak {1} 'den büyük olmayan bir sayısal değer girmeniz gerekiyor.";
+$l['warnings_error_invalid_expires_period'] = "Geçersiz bir son kullanma türü girdiniz.";
