443f145488e51e4e39039d8e411eb532ecea0d7d
Medet Erdal <tedemdev@gmail.com>
2021-03-09 12:20:51 +0300

    feat: done datahandler_post.lang.php

--- inc/languages/turkish/datahandler_post.lang.php
+++ inc/languages/turkish/datahandler_post.lang.php
@@ -1,30 +1,30 @@
 <?php
+
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Turkish Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['postdata_invalid_user_id'] = 'The user id does not exist. Please supply a valid user id.';
-$l['postdata_firstpost_no_subject'] = 'The thread does not have a subject. Please enter a subject.';
-$l['postdata_missing_subject'] = 'The subject is missing. Please enter a subject.';
-$l['postdata_missing_message'] = 'The message is missing. Please enter a message.';
-$l['postdata_message_too_long'] = 'The message is too long. Please enter a message shorter than {1} characters (currently {2}).';
-$l['postdata_message_too_short'] = 'The message is too short. Please enter a message longer than {1} characters.';
-$l['postdata_subject_too_long'] = 'The subject is too long. Please enter a subject shorter than 85 characters (currently {1}).';
-$l['postdata_banned_username'] = 'The username you entered may not be registered. Please enter a different username.';
-$l['postdata_bad_characters_username'] = 'The username you entered contains bad characters. Please enter a different username.';
-$l['postdata_invalid_username_length'] = 'The username you entered is of invalid length. Please enter a username between {1} and {2} characters.';
-$l['postdata_username_exists'] = 'The username you entered already exists. Please enter a different username.';
-$l['postdata_post_flooding'] = 'You are trying to post a message too quickly after posting a previous message. Please wait {1} more seconds.';
-$l['postdata_post_flooding_one_second'] = 'You are trying to post a message too quickly after posting a previous message. Please wait 1 more second.';
-$l['postdata_too_many_images'] = 'The message you entered contained {1} images, while only {2} images per message are allowed. Please lower the image count in your message to meet the limit.';
-$l['postdata_too_many_videos'] = 'The message you entered contained {1} videos, while only {2} videos per message are allowed. Please lower the video count in your message to meet the limit.';
-$l['postdata_invalid_prefix'] = 'The selected prefix is invalid. Please select a valid prefix.';
-$l['postdata_require_prefix'] = 'This forum requires a thread prefix to be set. Please select a valid prefix.';
+$l['postdata_invalid_user_id'] = 'Kullanıcı kimliği mevcut değil. Lütfen geçerli bir kullanıcı kimliği sağlayın.';
+$l['postdata_firstpost_no_subject'] = 'Konunun başlığı yok. Lütfen bir başlık girin.';
+$l['postdata_missing_subject'] = 'Başlık eksik. Lütfen bir başlık girin.';
+$l['postdata_missing_message'] = 'Mesaj eksik. Lütfen bir mesaj girin.';
+$l['postdata_message_too_long'] = 'Mesaj çok uzun. Lütfen {1} karakterden daha kısa bir mesaj girin (şu anda {2}).';
+$l['postdata_message_too_short'] = 'Mesaj çok kısa. Lütfen {1} karakterden uzun bir mesaj girin.';
+$l['postdata_subject_too_long'] = 'Başlık çok uzun. Lütfen 85 karakterden kısa bir başlık girin (şu anda {1}).';
+$l['postdata_banned_username'] = 'Girdiğiniz kullanıcı adı kayıtlı olmayabilir. Lütfen farklı bir kullanıcı adı girin.';
+$l['postdata_bad_characters_username'] = 'Girdiğiniz kullanıcı adı kötü karakterler içeriyor. Lütfen farklı bir kullanıcı adı girin.';
+$l['postdata_invalid_username_length'] = 'Girdiğiniz kullanıcı adı geçersiz uzunlukta. Lütfen {1} ve {2} karakterleri arasında bir kullanıcı adı girin.';
+$l['postdata_username_exists'] = 'Girdiğiniz kullanıcı adı zaten mevcut. Lütfen farklı bir kullanıcı adı girin.';
+$l['postdata_post_flooding'] = 'Önceki bir mesajı gönderdikten sonra çok hızlı bir şekilde mesaj göndermeye çalışıyorsunuz. Lütfen {1} saniye daha bekleyin.';
+$l['postdata_post_flooding_one_second'] = 'Önceki bir mesajı gönderdikten sonra çok hızlı bir şekilde mesaj göndermeye çalışıyorsunuz. Lütfen 1 saniye daha bekleyin.';
+$l['postdata_too_many_images'] = 'Girdiğiniz mesaj {1} resim içeriyordu, ancak mesaj başına yalnızca {2} resme izin veriliyor. Sınırı karşılamak için lütfen mesajınızdaki resim sayısını azaltın.';
+$l['postdata_too_many_videos'] = 'Girdiğiniz mesaj {1} video içeriyordu, ancak mesaj başına yalnızca {2} videoya izin veriliyor. Sınırı karşılamak için lütfen mesajınızdaki video sayısını azaltın.';
+$l['postdata_invalid_prefix'] = 'Seçilen önek geçersiz. Lütfen geçerli bir önek seçin.';
+$l['postdata_require_prefix'] = 'Bu forum, bir konu önekinin tanımlanmasını gerektirir. Lütfen geçerli bir önek seçin.';
 
-$l['thread_closed'] = "Thread Closed";
-$l['thread_opened'] = "Thread Opened";
-$l['thread_stuck'] = "Thread Stuck";
-$l['thread_unstuck'] = "Thread Unstuck";
-
+$l['thread_closed'] = "Konu Kapalı";
+$l['thread_opened'] = "Konu Açıldı";
+$l['thread_stuck'] = "Konu Birleşmiş";
+$l['thread_unstuck'] = "Konu Ayrılmış";
