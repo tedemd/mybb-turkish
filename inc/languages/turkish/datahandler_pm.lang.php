5dac0a0cb7223335368bdf8de6ae7a2f09d0afa5
Medet Erdal <tedemdev@gmail.com>
2021-03-09 12:13:39 +0300

    feat: done datahandler_pm.lang.php

--- inc/languages/turkish/datahandler_pm.lang.php
+++ inc/languages/turkish/datahandler_pm.lang.php
@@ -1,22 +1,23 @@
 <?php
+
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Turkish Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['pmdata_too_long_subject'] = 'The subject of your private message is too long. Please enter a shorter subject.';
-$l['pmdata_code_no_subject'] = '[no subject]';
-$l['pmdata_missing_subject'] = 'You did not enter a subject for your message. Please enter a subject.';
-$l['pmdata_missing_message'] = 'You did not enter a message to be sent. Please enter a message.';
-$l['pmdata_message_too_long'] = 'The message is too long. Please enter a message shorter than {1} characters (currently {2}).';
-$l['pmdata_invalid_recipients'] = "One or more of the recipients you specified are not registered usernames. Invalid recipients: {1}";
-$l['pmdata_no_recipients'] = "You did not enter any recipients to send this message to. You must enter at least one username in the 'to' field.";
-$l['pmdata_too_many_recipients'] = "You are only allowed to send messages to {1} users at a time.";
-$l['pmdata_recipient_is_ignoring'] = "You cannot send messages to {1} because you're on their ignore list.";
-$l['pmdata_recipient_has_buddy_only'] = "You cannot send messages to {1} because you're not on their buddy list.";
-$l['pmdata_recipient_pms_disabled'] = '{1} has private messaging disabled. You cannot send private messages to this user.';
-$l['pmdata_recipient_reached_quota'] = '{1} has reached their private message quota so your message could not be sent.';
+$l['pmdata_too_long_subject'] = 'Özel mesajınızın konusu çok uzun. Lütfen daha kısa bir konu girin.';
+$l['pmdata_code_no_subject'] = '[konu yok]';
+$l['pmdata_missing_subject'] = 'Mesajınız için konu girmediniz. Lütfen bir konu girin.';
+$l['pmdata_missing_message'] = 'Gönderilecek bir mesaj girmediniz. Lütfen bir mesaj giriniz.';
+$l['pmdata_message_too_long'] = 'Mesaj çok uzun. Lütfen {1} karakterden daha kısa bir mesaj girin (şu anda {2}).';
+$l['pmdata_invalid_recipients'] = "Belirttiğiniz bir veya daha fazla alıcı kayıtlı kullanıcı adı değil. Geçersiz alıcılar: {1}";
+$l['pmdata_no_recipients'] = "Bu mesajı göndermek için herhangi bir alıcı girmediniz. 'Kime' alanına en az bir kullanıcı adı girmelisiniz.";
+$l['pmdata_too_many_recipients'] = "Bir seferde yalnızca {1} kullanıcıya mesaj göndermenize izin verilir.";
+$l['pmdata_recipient_is_ignoring'] = "Yoksayılanlar listesinde olduğunuz için {1} 'e mesaj gönderemezsiniz.";
+$l['pmdata_recipient_has_buddy_only'] = "Arkadaş listesinde olmadığınız için {1} adlı kişiye mesaj gönderemezsiniz.";
+$l['pmdata_recipient_pms_disabled'] = '{1} özel mesajlaşmayı devre dışı bıraktı. Bu kullanıcıya özel mesaj gönderemezsiniz.';
+$l['pmdata_recipient_reached_quota'] = '{1} özel mesaj kotasına ulaştı, bu nedenle mesajınız gönderilemedi.';
 
-$l['pmdata_pm_flooding'] = 'You are trying to send a message too quickly after sending a previous message. Please wait {1} more seconds.';
-$l['pmdata_pm_flooding_one_second'] = 'You are trying to send a message too quickly after sending a previous message. Please wait 1 more second.';
+$l['pmdata_pm_flooding'] = 'Önceki bir mesajı gönderdikten sonra çok hızlı bir mesaj göndermeye çalışıyorsunuz. Lütfen {1} saniye daha bekleyin.';
+$l['pmdata_pm_flooding_one_second'] = 'Önceki bir mesajı gönderdikten sonra çok hızlı bir mesaj göndermeye çalışıyorsunuz. Lütfen 1 saniye daha bekleyin.';
